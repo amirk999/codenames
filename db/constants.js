9d0427957990452a118fba51a31f92713d5a5c93
Amir Karbasi <amirk999@gmail.com>
2021-01-01 20:12:52 -0500

    added DB constants

---
+++ db/constants.js
@@ -0,0 +1,20 @@
+const CardStatus = {
+    TEMPLATE: 1,
+    AVAILABLE: 2,
+    REVEALED: 3
+}
+
+const Color = {
+    RED: 1,
+    BLUE: 2,
+    BLACK: 3,
+    GRAY: 4
+}
+
+const GameStatus = {
+    INPROGRESS: 1,
+    REDWON: 2,
+    BLUEWON: 3
+}
+
+module.exports = { CardStatus, Color, GameStatus };
