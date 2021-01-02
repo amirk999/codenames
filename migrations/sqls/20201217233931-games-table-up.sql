906e4484625cc7bee8b264e48ec4a027b4a76d19
Amir Karbasi <amirk999@gmail.com>
2021-01-01 20:40:08 -0500

    Updated game column names

--- migrations/sqls/20201217233931-games-table-up.sql
+++ migrations/sqls/20201217233931-games-table-up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS games (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	status SMALLINT,
-	red_remaining SMALLINT,
-	blue_remaining SMALLINT,
-	current_turn SMALLINT
+	redRemaining SMALLINT,
+	blueRemaining SMALLINT,
+	currentTurn SMALLINT
 );
